06fffa4e2f646312e334fc1400ff7f454f26b641
Kshitij Upadhyay <upadhyay.k@gmail.com>
2015-10-16 13:03:17 +0100

    We really need integration tests here. Sorted a query

--- src/main/java/com/pigtrax/batch/dao/BarnDaoImpl.java
+++ src/main/java/com/pigtrax/batch/dao/BarnDaoImpl.java
@@ -1,5 +1,6 @@
 package com.pigtrax.batch.dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,7 +32,12 @@ public class BarnDaoImpl implements BarnDao {
 		qryBuffer.append("select id from pigtrax.\"Barn\" where \"barnId\" = ?");
 		final String qry = qryBuffer.toString();
 		if (barnId != null) {
-			retValList = jdbcTemplate.query(qry, new RowMapper<Integer>() {
+			retValList = jdbcTemplate.query(qry, new PreparedStatementSetter() {
+				@Override
+				public void setValues(PreparedStatement ps) throws SQLException {
+					ps.setString(1, barnId.toString());
+				}
+			}, new RowMapper<Integer>() {
 				@Override
 				public Integer mapRow(ResultSet rs, int arg1) throws SQLException {
 					return rs.getInt(1);
--- src/main/java/com/pigtrax/batch/dao/PenDaoImpl.java
+++ src/main/java/com/pigtrax/batch/dao/PenDaoImpl.java
@@ -1,5 +1,6 @@
 package com.pigtrax.batch.dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +31,12 @@ public class PenDaoImpl implements PenDao {
 		qryBuffer.append("select id from pigtrax.\"Pen\" where \"penId\" = ?");
 		final String qry = qryBuffer.toString();
 		if (penId != null) {
-			retValList = jdbcTemplate.query(qry, new RowMapper<Integer>() {
+			retValList = jdbcTemplate.query(qry, new PreparedStatementSetter() {
+				@Override
+				public void setValues(PreparedStatement ps) throws SQLException {
+					ps.setString(1, penId.toString());
+				}
+			}, new RowMapper<Integer>() {
 				@Override
 				public Integer mapRow(ResultSet rs, int arg1) throws SQLException {
 					return rs.getInt(1);

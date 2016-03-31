6a433c637ecd8c2cb848a01661c5b0a2c4dd2d22
Ankush <ankush.patni@gmail.com>
2016-03-31 08:11:46 +0530

    no message

--- src/main/java/com/pigtrax/batch/core/BulkUploadBatchTest.java
+++ src/main/java/com/pigtrax/batch/core/BulkUploadBatchTest.java
@@ -18,15 +18,15 @@ public class BulkUploadBatchTest {
 	private static void execute(final ApplicationContext context) {
 		Process processEngine = context.getBean(ProcessEngine.class);
 		Map<String, Object> inputMap = new HashMap<String, Object>();
-		inputMap.put(Constants.EVENT_TYPE, BatchType.SALESEVENTDETAILS);
-		inputMap.put(Constants.DATA, "C:\\ankush\\Elance\\Khitij\\xls\\SalesEventPig.csv");
+		inputMap.put(Constants.EVENT_TYPE, BatchType.REMOVALEVENTEXCEPTSALESEVENT);
+		inputMap.put(Constants.DATA, "C:\\Users\\ANKUSH\\Downloads\\Mortality&Adjustment-Group.csv");
 		inputMap.put(Constants.HEADER, "true");
 		inputMap.put(Constants.SEPERATOR, ",");
 		inputMap.put(Constants.FILE_TYPE, "csv");
 		inputMap.put(Constants.USER_NAME, "pigtraxsuperadmin");
 		
-		inputMap.put(Constants.COMPANY_ID, 3);
-		inputMap.put(Constants.PREMISE_ID,3);
+		inputMap.put(Constants.COMPANY_ID, 5);
+		inputMap.put(Constants.PREMISE_ID,17);
 		
 		processEngine.execute(inputMap);
 	}
